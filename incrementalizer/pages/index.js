707a049d1f0a1aee85115afb881dc7197d4f1b71
James Marsh <marshrjames@gmail.com>
2022-02-23 07:48:28 -0700

    basic app buildout

---
+++ incrementalizer/pages/index.js
@@ -0,0 +1,14 @@
+import { Heading, Page } from "@shopify/polaris";
+
+export default function Index() {
+  return (
+    <Page>
+      <Heading>
+        Shopify app with Node and React{" "}
+        <span role="img" aria-label="tada emoji">
+          🎉
+        </span>
+      </Heading>
+    </Page>
+  );
+}
