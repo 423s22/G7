14f5d9448d7d63efd5333fe67777aba925305705
riley slater <rileymail6@gmail.com>
2022-03-10 23:41:15 -0700

    adding resource picker on click

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,27 @@
 import { Page } from "@shopify/polaris";
+import { ResourcePicker } from "@shopify/app-bridge-react";
 import React from "react";
 
 class Index extends React.Component {
+  state = { open: false }
   render() {
     return (
       <Page
         title="Products"
         primaryAction={{
           content: "Select Products",
-          onAction: () => console.log("Hello")
+          onAction: () => this.setState({open: true})
         }}
-      />
+      >
+        <ResourcePicker 
+          resourceType='Product'
+          open={this.state.open}
+        />
+      </Page>
     )
   }
 }
+
 
 export default Index;
 /* 
